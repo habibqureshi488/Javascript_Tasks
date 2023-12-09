a1f01f55c70a50214fc967f9f9c96709242b7f2e
Habib <129363789+habibqureshi488@users.noreply.github.com>
2023-12-09 15:39:35 +0500

    Add files via upload

---
+++ Sulotion Box/Problem 1/plus_minus.js
@@ -0,0 +1,12 @@
+let pos = 0, neg = 0 , zero = 0 , length = arr.length
+arr.forEach( n => {
+    if( n > 0 )
+        pos++
+    else if( n < 0 )
+        neg++
+    else
+        zero++
+} )
+console.log( (pos / length).toFixed(6) )
+console.log( (neg / length).toFixed(6) )
+console.log( (zero / length).toFixed(6) )
